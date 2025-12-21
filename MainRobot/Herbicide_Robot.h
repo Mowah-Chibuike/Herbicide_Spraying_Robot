6bc088a655ffa0ae46858f8f122859441e548b85
Mowah Chibuike <mctimmykings@gmail.com>
2025-12-21 19:26:34 +0100

    update the MainRobot code to allow for WiFi addition and update in real time

--- MainRobot/Herbicide_Robot.h
+++ MainRobot/Herbicide_Robot.h
@@ -11,15 +11,9 @@
 #include <ESPmDNS.h>
 #include <BTS7960.h>
 #include <Arduino_JSON.h>
+#include <ArduinoJson.h>
 
-
-const char ssid[] = "Timothy's A16";
-const char password[] = "nmg9h28vmhjth6m";
-
-// const char *mqtt_server = "1db849a17f3f42baa8c4d1b41daf6b1a.s1.eu.hivemq.cloud";
-// const int mqtt_port = 8883;
-
-// const char *mqtt_user = "Mctimm";
-// const char *mqtt_pass = "Umbrelius12";
+String accessPoint = "Herbicide Spraying Robot";
+String accessPass = "password123";
 
 #endif /* _HERBICIDE_ROBOT_H */
--- MainRobot/MainRobot.ino
+++ MainRobot/MainRobot.ino
@@ -41,21 +41,15 @@ AsyncWebSocket ws("/ws");
 Preferences pref;
 
 JSONVar readings;
+JSONVar payload;
 
 float volume = 0;
 float voltage = 0;
+int speed;
 
-String accessPoint = "ESP32_Config";
-String accessPass = "password";
+String ssids[MAX_NETWORKS] = {"", "", "", "", ""};
+String passwords[MAX_NETWORKS] = {"", "", "", "", ""};
 
-String ssids[MAX_NETWORKS];
-String passwords[MAX_NETWORKS];
-
-String admin_user = "admin";
-String admin_password = "admin";
-
-bool configMode = false;
-bool isAuthenticated = false;
 
 unsigned long cleanupPrevMillis = 0;
 unsigned long readingsPrevMillis = 0;
@@ -88,52 +82,193 @@ void saveCredentials(String ssid, String password) {
 }
 
 void connectToWiFi() {
+  WiFi.disconnect(true);   // full reset
+  delay(500);
+
   for (int i = 0; i < MAX_NETWORKS; i++) {
-    if (ssids[i] != "") {
-      Serial.printf("Trying WiFi: %s\n", ssids[i].c_str());
-      WiFi.begin(ssids[i].c_str(), passwords[i].c_str());
-      if (WiFi.waitForConnectResult() == WL_CONNECTED) {
-        Serial.println("Connected!");
-        Serial.println(WiFi.localIP());
-        return;
-      }
+    if (ssids[i].length() == 0) continue;
+
+    Serial.printf("Trying WiFi: %s\n", ssids[i].c_str());
+
+    WiFi.begin(ssids[i].c_str(), passwords[i].c_str());
+
+    unsigned long startAttemptTime = millis();
+
+    while (WiFi.status() != WL_CONNECTED &&
+           millis() - startAttemptTime < 10000) {
+      delay(300);
+      Serial.print(".");
     }
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("\nConnected!");
+      Serial.println(WiFi.localIP());
+      return;
+    }
+
+    Serial.println("\nFailed, disconnecting...");
+    WiFi.disconnect(true);
+    delay(1000);
   }
 
   Serial.println("No known WiFi networks reachable");
 }
 
+void onAddCredential(AsyncWebServerRequest *request, uint8_t * data, size_t len, size_t index, size_t total) {
+  if (index + len != total) return;
+
+  StaticJsonDocument<512> received;
+  DeserializationError err = deserializeJson(received, data, total);
+
+  if (err) {
+    request->send(400, "text/plain", "Invalid JSON");
+    return;
+  }
+
+  String newSsid = received["ssid"];
+  String newPassword = received["pass"];
+
+  Serial.print("New SSID: ");
+  Serial.println(newSsid);
+
+  Serial.print("New Password: ");
+  Serial.println(newPassword);
+
+  saveCredentials(newSsid, newPassword);
+
+  StaticJsonDocument<256> send;
+    JsonArray SSIDs = send.createNestedArray("ssids");
+
+    for (int i = 0; i < MAX_NETWORKS; i++) {
+      if (ssids[i].length() > 0) {
+       SSIDs.add(ssids[i]);
+      }
+    }
+
+    send["max-num"] = MAX_NETWORKS;
+
+    String jsonString;
+    serializeJson(send, jsonString);
+
+  request->send(200, "application/json", jsonString);
+}
+
+void onDeleteCredential(AsyncWebServerRequest *request, uint8_t * data, size_t len, size_t index, size_t total) {
+  if (index + len != total) return;
+
+  StaticJsonDocument<512> received;
+  DeserializationError err = deserializeJson(received, data, total);
+
+  if (err) {
+    request->send(400, "text/plain", "Invalid JSON");
+    return;
+  }
+
+  String deleteSsid = received["ssid"];
+  pref.begin("wifi", false);
+  for (int i = 0; i < MAX_NETWORKS; i++) {
+    if (ssids[i] == deleteSsid) {
+      Serial.println(deleteSsid);
+      pref.remove(("ssid" + String(i)).c_str());
+      pref.remove(("pass" + String(i)).c_str());
+    }
+  }
+  pref.end();
+
+  loadCredentials();
+  StaticJsonDocument<256> send;
+    JsonArray SSIDs = send.createNestedArray("ssids");
+
+    for (int i = 0; i < MAX_NETWORKS; i++) {
+      if (ssids[i].length() > 0) {
+       SSIDs.add(ssids[i]);
+      }
+    }
+ 
+    send["max-num"] = MAX_NETWORKS;
+
+    String jsonString;
+    serializeJson(send, jsonString);
+
+  request->send(200, "application/json", jsonString);
+}
+
+void onEditCredential(AsyncWebServerRequest *request, uint8_t * data, size_t len, size_t index, size_t total) {
+  if (index + len != total) return;
+
+  StaticJsonDocument<512> received;
+  DeserializationError err = deserializeJson(received, data, total);
+
+  if (err) {
+    request->send(400, "text/plain", "Invalid JSON");
+    return;
+  }
+
+  String editSsid = received["ssid"];
+  String newPassword = received["pass"];
+  pref.begin("wifi", false);
+  for (int i = 0; i < MAX_NETWORKS; i++) {
+    if (ssids[i] == editSsid) {
+      Serial.println(editSsid);
+      pref.putString(("pass" + String(i)).c_str(), newPassword);
+    }
+  }
+  pref.end();
+
+  loadCredentials();
+  StaticJsonDocument<256> send;
+    JsonArray SSIDs = send.createNestedArray("ssids");
+
+    for (int i = 0; i < MAX_NETWORKS; i++) {
+      if (ssids[i].length() > 0) {
+       SSIDs.add(ssids[i]);
+      }
+    }
+ 
+    send["max-num"] = MAX_NETWORKS;
+
+    String jsonString;
+    serializeJson(send, jsonString);
+
+  request->send(200, "application/json", jsonString);
+}
+
 void setupWebServer() {
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    if (isAuthenticated) {
-      request->send(LittleFS, "/index.html", String(), false);
-    } else {
-      request->send(LittleFS, "/login.html", String(), false);
+  server.on("/list-credentials", HTTP_GET, [](AsyncWebServerRequest *request) {
+    StaticJsonDocument<256> doc;
+    JsonArray SSIDs = doc.createNestedArray("ssids");
+
+    for (int i = 0; i < MAX_NETWORKS; i++) {
+      if (ssids[i].length() > 0) {
+       SSIDs.add(ssids[i]);
+      }
     }
+
+    doc["max-num"] = MAX_NETWORKS;
+
+    String jsonString;
+    serializeJson(doc, jsonString);
+    request->send(200, "application/json", jsonString); 
   });
 
-  server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/styles.css", "text/css");
-  });
+  server.on("/add-credential", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, onAddCredential);
+  server.on("/delete-credential", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, onDeleteCredential);
+  server.on("/edit-credential", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, onEditCredential);
 
-  server.on("/login.js", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/login.js", "application/javascript");
-  });
+  // server.on("/login", HTTP_POST, [](AsyncWebServerRequest *request) {
+  //   String username, password;
+  //   if (request->hasParam("username", true)) username = request->getParam("username", true)->value();
+  //   if (request->hasParam("password", true)) password = request->getParam("password", true)->value();
 
-  server.on("/login", HTTP_POST, [](AsyncWebServerRequest *request) {
-    String username, password;
-    if (request->hasParam("username", true)) username = request->getParam("username", true)->value();
-    if (request->hasParam("password", true)) password = request->getParam("password", true)->value();
+  //   if (username == "admin" && password == "admin") {
+  //     isAuthenticated = true;
+  //     request->redirect("/");
+  //   } else {
+  //     request->send(403, "text/plain", "Invalid credentials");
+  //   }
+  // });
 
-    if (username == "admin" && password == "admin") {
-      isAuthenticated = true;
-      request->redirect("/");
-    } else {
-      request->send(403, "text/plain", "Invalid credentials");
-    }
-  });
-
-  server.begin();
+  // server.begin();
 }
 
 void notifyClients(String message) {
@@ -163,21 +298,13 @@ float getPesticideLevel() {
   // Generate percentage of fluid present in the container
   percentage = map(distanceCm, MIN_PESTICIDE_HGT, MAX_PESTICIDE_HGT, 0, 100);
 
-  Serial.print("Percentage (%): ");
-  Serial.println(percentage);
-
   return percentage;
 }
 
 float getVoltage() {
   int sensorValue = analogRead(VOLT_SENSOR);
 
-  Serial.println("SensorValue: ");
-  Serial.println(sensorValue);
-
   float voltage = sensorValue * (11.34 / 2768);
-  Serial.print("Voltage: ");
-  Serial.println(voltage);
 
   return (voltage);
 }
@@ -185,6 +312,7 @@ float getVoltage() {
 String getSensorReadings() {
   readings["volume"] = String(volume);
   readings["battery"] = String(voltage);
+  readings["speed"] = String(speed);
 
   String jsonString = JSON.stringify(readings);
   return jsonString;
@@ -194,19 +322,19 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   String msg = String((char*)data, len);
 
   if (msg == "forward") {
-    motor1.pwm = motor2.pwm = 255;
+    motor1.pwm = motor2.pwm = speed;
     motor1.front();
     motor2.front();
   } else if (msg == "backward") {
-    motor1.pwm = motor2.pwm = 200;
+    motor1.pwm = motor2.pwm = speed;
     motor1.back();
     motor2.back();
   } else if (msg == "left") {
-    motor2.pwm = 200;
+    motor2.pwm = speed;
     motor1.stop();
     motor2.front();
   } else if (msg == "right") {
-    motor1.pwm = 200;
+    motor1.pwm = speed;
     motor1.front();
     motor2.stop();
   } else if (msg == "stop") {
@@ -214,13 +342,37 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
     motor2.stop();
   } else if (msg == "spray") {
     Serial.println("spray");
-    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(RELAY_PIN, LOW);
   } else if (msg == "stop-spray") {
     Serial.println("spray-stop");
-    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN, HIGH);
   } else if (msg == "getReadings") {
     String sensorReadings = getSensorReadings();
     notifyClients(sensorReadings);
+  } else if (msg.indexOf("speedVal") != -1) {
+    Serial.println(msg);
+    int val = msg.substring(9).toInt();
+    Serial.println(val);
+    speed = map(val, 0, 100, 150, 255);
+  } else if (msg.startsWith("{")) {
+    StaticJsonDocument<128> doc;
+    DeserializationError err = deserializeJson(doc, msg);
+
+    if (!err && doc.containsKey("type")) {
+      const char* type = doc["type"];
+
+      if (strcmp(type, "ping") == 0) {
+        StaticJsonDocument<128> resp;
+        resp["type"] = "pong";
+        resp["t"] = doc["t"];   // echo timestamp back
+
+        String json;
+        serializeJson(resp, json);
+
+        ws.textAll(json);   // respond immediately
+        return;
+      }
+    }
   }
 }
 
@@ -253,10 +405,6 @@ void initWebSocket() {
 void setup() {
   Serial.begin(9600);
   while (!Serial) {;};
-  // if (!LittleFS.begin()) {
-  //   Serial.println("LittleFS mount failed!");
-  //   return;
-  // }
 
   motor1.begin(); motor1.enable();
   motor2.begin(); motor2.enable();
@@ -267,22 +415,23 @@ void setup() {
   pinMode(VOLT_SENSOR, INPUT);
 
   digitalWrite(TRIG_PIN, LOW);
-  digitalWrite(RELAY_PIN, LOW);
-  if (configMode) {
-    Serial.println("======= Config Mode ========");
-    WiFi.softAP(accessPoint, accessPass);
-    Serial.println(WiFi.softAPIP());
+  digitalWrite(RELAY_PIN, HIGH);
 
     setupWebServer();
-  } else {
-    Serial.println("======= Running Mode ========");
+
+    loadCredentials();
+
+    WiFi.mode(WIFI_AP_STA);    
     connectToWiFi();
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) delay(500);
+
+    WiFi.softAP(accessPoint, accessPass);
+    Serial.print("AP IP Adress: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println(WiFi.localIP());
+    setupWebServer();
 
     initWebSocket();
-    server.begin();
-  }
 
   if (!MDNS.begin("main-robot")) { // Connect to http://main-robot.local 
     Serial.println("Error setting up MDNS responder!");
@@ -293,19 +442,30 @@ void setup() {
     
     Serial.println("mDNS responder started");
     Serial.println("Connect to web server by using http://main-robot.local");
-}
 
-bool doOnce = true;
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type");
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+    server.onNotFound([](AsyncWebServerRequest *request) {
+  if (request->method() == HTTP_OPTIONS) {
+    request->send(200);
+  } else {
+    request->send(404);
+  }
+});
+    server.begin();
+}
 
 void loop() {
    unsigned long now = millis();
 
-  if (!configMode && (now - cleanupPrevMillis >= 1000)) {
+  if (now - cleanupPrevMillis >= 1000) {
     ws.cleanupClients();
     cleanupPrevMillis = millis();
   }
 
-  if (!configMode && (now - readingsPrevMillis >= 5000)) {
+  if (now - readingsPrevMillis >= 500) {
     volume = getPesticideLevel();
     voltage = getVoltage();
 
